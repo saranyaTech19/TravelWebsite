fca12c103162b6e08796b1cc52b15869e0c8ea55
tech19 <tech19@thevatconsultant.com>
2026-03-19 18:34:10 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -16,8 +16,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middlewares
 app.use(cors({
-  origin: ['*'],
-  credentials: true
+  origin: '*'
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
